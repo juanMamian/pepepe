210a607e57efb1ea4cce89ab1305db6830769ff9
Juan Sebastian Mamian Lopez <juancho.mamian@gmail.com>
2021-06-27 14:38:25 -0500

    creando diagramaTotalTrabajos

---
+++ vuecliente/src/components/diagramaTotalTrabajos/DiagramaTotalTrabajos.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="diagramaTotalTrabajos">
+      <div id="contenedorNodos">
+          
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "DiagramaTotalTrabajos"
+}
+</script>
+
+<style scoped>
+
+</style>
